0e6d91453aec3881dd8e1819be9f6fe3f7a24ffc
BlackJack7 <BlackJack7@gmail.com>
2015-05-30 14:55:18 +0300

    server

---
+++ StarTeam/StarTeamApp/Server/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StarTeamApp.Server
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            InitMongo.A();
+        }
+    }
+}
